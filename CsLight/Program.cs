a6a74205a727673e316a949f4a0000ea4f171506
gekyume <herman.tsoy@gmail.com>
2023-09-06 23:20:14 +1000

    2s

--- CsLight/Program.cs
+++ CsLight/Program.cs
@@ -1,4 +1,17 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsLight
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
             int oldLadies;
             int receptionTime = 10;
             int timePerMinute = 60;
@@ -10,4 +23,10 @@
             int waitingTimeHours = allWaitingTimeInMinutes / timePerMinute;
             int waitingTimeMinutes = allWaitingTimeInMinutes % timePerMinute;
             Console.WriteLine($"Вы должны отстоять в очереди {waitingTimeHours} часа и {waitingTimeMinutes} минут");
- 
+            Console.WriteLine("fds");
+            Console.WriteLine("dsdfsfds");
+
+
+        }
+    }
+}
