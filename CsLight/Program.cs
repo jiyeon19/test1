5080954b1de420c1d35d0aa8dee21cd04a47614b
gekyume <herman.tsoy@gmail.com>
2023-09-06 18:01:26 +1000

    first commit

---
+++ CsLight/Program.cs
@@ -0,0 +1,13 @@
+﻿
+            int oldLadies;
+            int receptionTime = 10;
+            int timePerMinute = 60;
+
+            Console.Write("Введите количество старушек: ");
+            oldLadies = Convert.ToInt32(Console.ReadLine());
+
+            int allWaitingTimeInMinutes = oldLadies * receptionTime;
+            int waitingTimeHours = allWaitingTimeInMinutes / timePerMinute;
+            int waitingTimeMinutes = allWaitingTimeInMinutes % timePerMinute;
+            Console.WriteLine($"Вы должны отстоять в очереди {waitingTimeHours} часа и {waitingTimeMinutes} минут");
+ 
